893cc029a801458ab0aebd2d7aa69a5a94c6d1a1
tama <tama@ttk1.net>
2019-07-11 02:25:16 +0900

    ポート番号の環境変数化

--- src/hoge.js
+++ src/hoge.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const server = http.createServer();
+const port = process.env.PORT || 3000;
 
 server.on('request', (req, res) => {
     res.write('hello, world');
@@ -8,4 +9,4 @@ server.on('request', (req, res) => {
     console.log(req.url);
 })
 
-server.listen(3000);
+server.listen(port);
